0b6dcfb2e28104702327e02893bdec38bd6b86a6
Palemoky <palemoky@gmail.com>
2025-11-12 11:28:17 +0800

    feat: refactor main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"github.com/palemoky/fight-the-landlord-go/internal/ui"
-	"github.com/palemoky/fight-the-landlord-go/internal/game"
 )
 
 func main() {
-	// 创建一个UI实例
-	terminalUI := ui.NewTerminalUI()
-
-	// 创建游戏实例，并将 UI 注入进去
-	doudizhuGame := game.NewGame(terminalUI)
-
-	// 启动游戏
-	doudizhuGame.Run()
+	ui.Start()
 }
